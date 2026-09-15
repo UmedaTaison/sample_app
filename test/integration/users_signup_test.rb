a35e9139a52d6499b73e87ea7030d7b5916db8c8
梅田汰勇 <umeda-ta2604@ibjapan.jp>
2026-09-15 15:44:46 +0900

    Implement basic login

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -22,8 +22,8 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     end
     follow_redirect!
     assert_response :success
-    assert_not flash.empty?
     assert_select "title", "Example User | Ruby on Rails Tutorial Sample App"
+    assert is_logged_in?
   end
 end
 
